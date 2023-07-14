e86c9c0151a8f1c508759bce69b853764306aff6
stevier biko <stevierbiko@gmail.com>
2023-07-14 17:19:15 +0300

    m

---
+++ build/precache-manifest.80f5f049972702c0fc841e44f3c034bc.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c8a5fdb854feacdd895",
+    "url": "/static/css/main.3071b5de.chunk.css"
+  },
+  {
+    "revision": "4c8a5fdb854feacdd895",
+    "url": "/static/js/main.2fdc1306.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1e3eb9936c07feb8d585",
+    "url": "/static/js/2.a542f060.chunk.js"
+  },
+  {
+    "revision": "19f6a194b3473a3fa6c3f39c06b0d872",
+    "url": "/static/media/banner.19f6a194.jpg"
+  },
+  {
+    "revision": "e6340b42d58670756b923939847a7226",
+    "url": "/static/media/Staff1.e6340b42.jpeg"
+  },
+  {
+    "revision": "2c37a8f1333168d801ab5aadcfc08847",
+    "url": "/static/media/banner5.2c37a8f1.jpg"
+  },
+  {
+    "revision": "6ab4ca99fba000e13be2e7bcd8dded37",
+    "url": "/static/media/logo.6ab4ca99.png"
+  },
+  {
+    "revision": "9dc7e60afcdcc60f84098bfe600b42f2",
+    "url": "/static/media/Parish1.9dc7e60a.jpeg"
+  },
+  {
+    "revision": "0f2471c57b82eeafa474a084fa4e743b",
+    "url": "/static/media/Staff4.0f2471c5.jpeg"
+  },
+  {
+    "revision": "f5dccf6d9b7851159156779a4a1709ea",
+    "url": "/static/media/bg1.f5dccf6d.png"
+  },
+  {
+    "revision": "169e76642545d9018c49ec310b6faa4a",
+    "url": "/static/media/about1.169e7664.jpeg"
+  },
+  {
+    "revision": "009954fc0bb665169d3e0b6ce97300cd",
+    "url": "/static/media/bishop2.009954fc.jpg"
+  },
+  {
+    "revision": "1be41f42be475d0822238a22bfe7224d",
+    "url": "/static/media/about2.1be41f42.jpeg"
+  },
+  {
+    "revision": "f8f920811242140b1f186177f8b62261",
+    "url": "/static/media/banner2.f8f92081.jpg"
+  },
+  {
+    "revision": "2fe71a4642fab8373f2af9c51c383173",
+    "url": "/static/media/banner3.2fe71a46.jpg"
+  },
+  {
+    "revision": "4e1d757d3a8f6489d27ff12e09e5889d",
+    "url": "/static/media/st-anthony-404.4e1d757d.png"
+  },
+  {
+    "revision": "adb6bc850df76d2e039d57e9987077b6",
+    "url": "/static/media/banner4.adb6bc85.jpg"
+  },
+  {
+    "revision": "d8c8d1cf2c1744542d875edd43137081",
+    "url": "/static/media/Staff2.d8c8d1cf.jpeg"
+  },
+  {
+    "revision": "a573bdf7b296f6e9e22ca7f0c0ae41d9",
+    "url": "/static/media/Staff3.a573bdf7.jpeg"
+  },
+  {
+    "revision": "9e46dcddc255f093ebfb546a5a16398c",
+    "url": "/static/media/Staff5.9e46dcdd.jpeg"
+  },
+  {
+    "revision": "795a6f39171aa81f49e256d38ae18e9c",
+    "url": "/static/media/Parish2.795a6f39.jpeg"
+  },
+  {
+    "revision": "38157775726d2ec6dedf33d93e66e84c",
+    "url": "/static/media/Parish3.38157775.jpeg"
+  },
+  {
+    "revision": "d8ede863ebfda384f76e7a568380cae2",
+    "url": "/static/media/Parish4.d8ede863.jpeg"
+  },
+  {
+    "revision": "9f7168fed09151e081d280ba55ebbcb6",
+    "url": "/static/media/Parish5.9f7168fe.jpeg"
+  },
+  {
+    "revision": "2730c079f5c77a498471f3bc8c4e554f",
+    "url": "/static/media/gallery1.2730c079.jpeg"
+  },
+  {
+    "revision": "47f32bd57cbfd9a92a6dfcef10c6d099",
+    "url": "/static/media/gallery10.47f32bd5.jpeg"
+  },
+  {
+    "revision": "1be41f42be475d0822238a22bfe7224d",
+    "url": "/static/media/gallery11.1be41f42.jpeg"
+  },
+  {
+    "revision": "34be4996b19d94526a24d1cdbf9824c8",
+    "url": "/static/media/gallery12.34be4996.jpeg"
+  },
+  {
+    "revision": "34852baeb16194a9efd8025d8c644ee0",
+    "url": "/static/media/gallery13.34852bae.jpeg"
+  },
+  {
+    "revision": "2f905b576af356078fb70a185053b934",
+    "url": "/static/media/gallery14.2f905b57.jpeg"
+  },
+  {
+    "revision": "c7c28702d5f0564a2bb63a4d1a658764",
+    "url": "/static/media/gallery15.c7c28702.jpeg"
+  },
+  {
+    "revision": "4784e179d435d871fe765ee5208c0825",
+    "url": "/static/media/gallery2.4784e179.jpeg"
+  },
+  {
+    "revision": "1c2c8da8a733dbcf2b0888fee6cdf97d",
+    "url": "/static/media/gallery3.1c2c8da8.jpeg"
+  },
+  {
+    "revision": "b8a19afab11e0dd61f0bbb56b4a91de3",
+    "url": "/static/media/gallery4.b8a19afa.jpeg"
+  },
+  {
+    "revision": "89f4826de6d598a88df0dbf58f5bc7d9",
+    "url": "/static/media/gallery5.89f4826d.jpeg"
+  },
+  {
+    "revision": "c415728d6df1260b43c9cfac1bc50362",
+    "url": "/static/media/gallery6.c415728d.jpeg"
+  },
+  {
+    "revision": "d3933f746241c856f2fb6a1d2fcdbce6",
+    "url": "/static/media/gallery7.d3933f74.jpeg"
+  },
+  {
+    "revision": "cceba1600ce63074bbc3248d33d222a2",
+    "url": "/static/media/gallery8.cceba160.jpeg"
+  },
+  {
+    "revision": "9b4bd66279a8992012cf88ddd7d6502c",
+    "url": "/static/media/gallery9.9b4bd662.jpeg"
+  },
+  {
+    "revision": "99f866daed67f5b15d7210da0a54ec6e",
+    "url": "/index.html"
+  }
+];
